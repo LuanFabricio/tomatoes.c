28a8a290803f16188097dda8744c47f095cf51b0
Luan Fabrício <luanlimaleite@hotmail.com>
2026-01-29 23:06:51 -0300

    feat: implements task form component

---
+++ include/components/task_form_component.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "raylib.h"
+
+#include "system/task.h"
+
+typedef enum {
+	TASK_FORM_NONE,
+	TASK_FORM_INPUT,
+	TASK_FORM_TASK_LEVEL,
+} task_form_input_e;
+
+typedef struct {
+	task_t task;
+
+	task_form_input_e input_state;
+
+	Rectangle form_rect;
+
+	Rectangle input_rect;
+	Rectangle task_level_rect;
+	Rectangle task_level_options_rect[TASK_LEVEL_LEN];
+	Rectangle completed_rect;
+
+	bool show_form;
+	bool show_task_level_selector;
+} task_form_t;
+
+void task_form_component_handle_input(task_form_t *form);
+void task_form_component_draw(const task_form_t *form);
---
+++ src/components/task_form_component.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "raylib.h"
+
+#include "components/task_form_component.h"
+#include "system/task.h"
+
+static void task_form__get_task_level_text(char *buffer, size_t buffer_size, task_level_e level)
+{
+	switch (level) {
+		case TASK_LEVEL_EASY:
+			strncpy(buffer, "Easy", buffer_size);
+			break;
+		case TASK_LEVEL_MEDIUM:
+			strncpy(buffer, "Medium", buffer_size);
+			break;
+		case TASK_LEVEL_HARD:
+			strncpy(buffer, "Hard", buffer_size);
+			break;
+		default: break;
+	}
+
+}
+
+static void task_form__component_handle_mouse(task_form_t *form)
+{
+	const Vector2 mouse = GetMousePosition();
+	if (form->input_state == TASK_FORM_TASK_LEVEL) {
+		for (size_t i = 0; i < TASK_LEVEL_LEN; i++) {
+			if (CheckCollisionPointRec(mouse, form->task_level_options_rect[i])) {
+				form->task.task_level = i;
+			}
+		}
+	}
+
+	form->input_state = TASK_FORM_NONE;
+	if (CheckCollisionPointRec(mouse, form->input_rect)) {
+		form->input_state = TASK_FORM_INPUT;
+	} else if (CheckCollisionPointRec(mouse, form->task_level_rect)) {
+		form->input_state = TASK_FORM_TASK_LEVEL;
+	} else if (CheckCollisionPointRec(mouse, form->completed_rect)) {
+		form->task.completed = !form->task.completed;
+	}
+}
+
+static void task_form__component_handle_keyboard(task_form_t *form, int key_pressed)
+{
+	if (form->input_state == TASK_FORM_INPUT) {
+		const size_t name_len = strlen(form->task.content);
+		if (name_len >= TASK_MAX_CONTENT_LEN) return;
+
+		form->task.content[name_len] = (char)key_pressed;
+	}
+}
+
+void task_form_component_handle_input(task_form_t *form)
+{
+	const int key_pressed = GetKeyPressed();
+	if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+		task_form__component_handle_mouse(form);
+	} else if(key_pressed != 0) {
+		task_form__component_handle_keyboard(form, key_pressed);
+	}
+}
+
+void task_form_component_draw(const task_form_t *form)
+{
+	if (!form->show_form) return;
+
+	DrawRectangleRec(form->form_rect, GREEN);
+
+	DrawRectangleRec(form->input_rect, WHITE);
+	DrawText(
+		form->task.content,
+		form->input_rect.x,
+		form->input_rect.y,
+		32,
+		BLACK
+	);
+
+	DrawRectangleRec(form->completed_rect, form->task.completed ? BLACK : WHITE );
+
+	DrawRectangleRec(form->task_level_rect, WHITE);
+	char task_level_text[20] = "";
+	const size_t task_level_text_size = sizeof(task_level_text);
+	task_form__get_task_level_text(
+		task_level_text,
+		task_level_text_size,
+		form->task.task_level);
+	DrawText(
+		task_level_text,
+		form->task_level_rect.x,
+		form->task_level_rect.y,
+		32,
+		BLACK);
+
+	if (form->input_state == TASK_FORM_TASK_LEVEL) {
+		for (size_t i = 0; i < TASK_LEVEL_LEN; i++) {
+			DrawRectangleRec(form->task_level_options_rect[i], WHITE);
+			task_form__get_task_level_text(
+				task_level_text,
+				task_level_text_size,
+				i);
+			DrawText(
+				task_level_text,
+				form->task_level_options_rect[i].x,
+				form->task_level_options_rect[i].y,
+				32,
+				BLACK);
+		}
+	}
+}
